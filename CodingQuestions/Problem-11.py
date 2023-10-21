eac85fe25053c79d3407c5918837dd4a1d890fc9
KarthikShetty27 <karthikshetty270902@gmail.com>
2023-10-21 11:50:11 +0530

    Problem-11: Problem Text

--- CodingQuestions/Problem-11.py
+++ CodingQuestions/Problem-11.py
@@ -1,3 +1,17 @@
+# You will be given an array arr of integers of length N.
+# You can construct an integer from two integers by treating the integers as strings and then concatenating them.
+# For example, 19 and 4 can be used to construct 194 and 419.
+# The task is to find whether it’s possible to construct an integer using all the digits of the numbers such that
+# it would be divisible by 3.
+# If it is possible then print 1 and if not print 0
+
+# For example:
+# INPUT:
+# N=2
+# arr={1, 4}
+# OUTPUT:
+# 0
+
 def is_divisible_by_3(arr):
     # Calculate the sum of all elements in the array
     total_sum = sum(arr)
